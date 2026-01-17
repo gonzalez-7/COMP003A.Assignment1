a0ea396f2990fb8eeb83987f5fd26b7da5b01b16
gonzalez-7 <20agonzalezp@gmail.com>
2026-01-16 19:55:44 -0800

    Add items 1–10

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. Thing1");
+            Console.WriteLine("2. Thing2");
+            Console.WriteLine("3. Thing3");
+            Console.WriteLine("4. Thing4");
+            Console.WriteLine("5. Thing5");
+            Console.WriteLine("6. Thing6");
+            Console.WriteLine("7. Thing7");
+            Console.WriteLine("8. Thing8");
+            Console.WriteLine("9. Thing9");
+            Console.WriteLine("10. Thing10");
+
         }
     }
 }
