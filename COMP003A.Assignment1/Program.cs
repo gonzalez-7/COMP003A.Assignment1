1e0507f918d1b0886f40b1c52232750a2604bbd9
gonzalez-7 <20agonzalezp@gmail.com>
2026-01-16 19:59:59 -0800

    Add items 11-20

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -15,7 +15,16 @@
             Console.WriteLine("8. Thing8");
             Console.WriteLine("9. Thing9");
             Console.WriteLine("10. Thing10");
-
-        }
+            Console.WriteLine("11. Thing11");
+            Console.WriteLine("12. Thing12");
+            Console.WriteLine("13. Thing13");
+            Console.WriteLine("14. Thing14");
+            Console.WriteLine("15. Thing15");
+			Console.WriteLine("16. Thing16");
+			Console.WriteLine("17. Thing17");
+			Console.WriteLine("18. Thing18");
+			Console.WriteLine("19. Thing19");
+			Console.WriteLine("20. Thing20");
+		}
     }
 }
